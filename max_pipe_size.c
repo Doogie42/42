53f757a9937a94940ccb4466556d1b954f2efe05
Doogie42 <144601539+Doogie42@users.noreply.github.com>
2024-02-09 16:10:24 +0100

    Add files via upload

---
+++ max_pipe_size.c
@@ -0,0 +1,24 @@
+#include "unistd.h"
+#include "stdio.h"
+#include "string.h"
+
+
+//65535 is max pipe sizr
+int main()
+{
+	int fd[2];
+	pipe(fd);
+	int nb_char_write = 66666;
+	for (size_t i = 0; i < nb_char_write; i++)
+	{
+		write(fd[1], "a", 1 );
+		printf("%zu\n", i);
+	}
+	write(fd[1], "z", 1 );
+
+	char buffer[77777];
+	bzero(buffer, 77777);
+
+	read(fd[0], buffer, 77777);
+	printf("%s\n", buffer);
+}
